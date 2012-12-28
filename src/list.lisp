a36c78915c65b0f4efec5db73c75edd41f2b1f5e
Tom Hulihan <hulihan.tom159@gmail.com>
2012-12-28 03:53:22 -0500

    no longer exporting unwanted functions

--- src/list.lisp
+++ src/list.lisp
@@ -3,32 +3,6 @@
 ;;; functions that are exported are par-map and par-map-chunked.
 
 (in-package #:parallel)
-  
-;;; The following few functions (take through flatten) are utilities for
-;;; chunking and flattening the list.
-
-;; Take up to n elements from a list.
-(defun take (n xs)
-  (unless (or (null xs) (= n 0))
-    (cons (car xs) (take (1- n) (cdr xs)))))
-
-;; Drop up to n elements from a list.
-(defun drop (n xs)
-  (if (or (null xs) (= n 0))
-    xs
-    (drop (1- n) (cdr xs))))
-
-;; Split a list into chunks.
-(defun chunk-list (n xs)
-  (if (< n (length xs))
-    (cons (take n xs) (chunk-list n (drop n xs))))
-    (cons xs nil))
-
-;; Flatten out a chunked list.
-(defun flatten (xs)
-  (cond ((null xs) nil)
-        ((atom xs) (list xs))
-        (t (mapcan #'flatten xs))))
 
 ;; This function computes a function upon a list (or lists) in parallel.
 (defun par-map (f xs &optional (max-threads 4))
@@ -45,8 +19,34 @@
     (recur nil nil xs)))
 
 
-;; Break a list up into `size` chunks, and process those chunks in parallel.
-(defun par-map-chunked (f size xs &optional (max-threads 4))
-  (flatten (par-map (lambda (ys) (mapcar (lambda (y) (funcall f y)) ys))
-                    (chunk-list size xs)
-                    max-threads)))
+  
+;; The following few functions (take through flatten) are utilities for
+;; chunking and flattening the list.
+
+         ; Take up to n elements from a list.
+(labels ((take (n xs)
+           (unless (or (null xs) (= n 0))
+             (cons (car xs) (take (1- n) (cdr xs)))))
+
+         ; Drop up to n elements from a list.
+         (drop (n xs)
+           (if (or (null xs) (= n 0))
+             xs
+             (drop (1- n) (cdr xs))))
+
+         ; Split a list into chunks.
+         (chunk-list (n xs)
+           (if (< n (length xs))
+             (cons (take n xs) (chunk-list n (drop n xs))))
+             (cons xs nil))
+
+         ; Flatten out a chunked list.
+         (flatten (xs)
+           (cond ((null xs) nil)
+                 ((atom xs) (list xs))
+                 (t (mapcan #'flatten xs)))))
+  ;; Break a list up into `size` chunks, and process those chunks in parallel.
+  (defun par-map-chunked (f size xs &optional (max-threads 4))
+    (flatten (par-map (lambda (ys) (mapcar (lambda (y) (funcall f y)) ys))
+                      (chunk-list size xs)
+                      max-threads))))
